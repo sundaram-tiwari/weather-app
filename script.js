c0b7cdec8ea4461b9c828e6e0c0a4d38a5b6198c
sundaram <sundaramtiwari04@gmail.com>
2025-01-05 11:04:38 +0530

    little change

--- script.js
+++ script.js
@@ -3,42 +3,40 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 
 const searchBox = document.querySelector('.search input');
 const searchBtn = document.querySelector('.btn i');
-const weatherImg = document.querySelector('.weather-icon')
+const weatherImg = document.querySelector('.weather-icon');
 
 const checkWeather = async (city) => {
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     var data = await response.json();
 
-    console.log(data)
-
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + '°C';
     document.querySelector(".humidity").innerHTML = data.main.humidity + '%';
-    document.querySelector(".wind").innerHTML = data.wind.speed + ' km/h'
+    document.querySelector(".wind").innerHTML = data.wind.speed + ' km/h';
 
     if (data.weather[0].main == 'Clear') {
-        weatherImg.src = 'images/clear.png'
+        weatherImg.src = 'images/clear.png';
     }
     else if (data.weather[0].main == 'Clouds') {
-        weatherImg.src = 'images/clouds.png'
+        weatherImg.src = 'images/clouds.png';
     }
     else if (data.weather[0].main == 'Drizzle') {
-        weatherImg.src = 'images/drizzle.png'
+        weatherImg.src = 'images/drizzle.png';
     }
     else if (data.weather[0].main == 'Humidity') {
-        weatherImg.src = 'images/humidity.png'
+        weatherImg.src = 'images/humidity.png';
     }
     else if (data.weather[0].main == 'Mist') {
-        weatherImg.src = 'images/mist.png'
+        weatherImg.src = 'images/mist.png';
     }
     else if (data.weather[0].main == 'Rain') {
-        weatherImg.src = 'images/rain.png'
+        weatherImg.src = 'images/rain.png';
     }
     else if (data.weather[0].main == 'Snow') {
-        weatherImg.src = 'images/snow.png'
+        weatherImg.src = 'images/snow.png';
     }
 }
 
 searchBtn.addEventListener('click', () => {
     checkWeather(searchBox.value);
-})
+});
